3c586ce93aea0e05bcb8696b01b99e85e86dddcd
abhay3690 <abhay7777@gmail.com>
2024-08-01 22:10:45 +0530

    ad

--- order-service/src/main/java/com/abhay/microservices/controller/OrderController.java
+++ order-service/src/main/java/com/abhay/microservices/controller/OrderController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
-    
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest){
